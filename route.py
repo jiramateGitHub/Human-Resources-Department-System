514e981248976eafa2f21e05ce731af7c682cb22
jiramateGitHub <jiramategithub@gmail.com>
2019-11-26 00:23:24 +0700

    update commit

--- route.py
+++ route.py
@@ -3,7 +3,7 @@ from flask import render_template
 from flask import request
 from flask import redirect
 from flask import url_for
-from datetime import timedelta, datetime, tzinfo
+from datetime import timedelta, datetime, tzinfo, date
 import pymysql
 
 app = Flask(__name__)
@@ -27,7 +27,6 @@ def about_employee():
         rows_count_employee = cur.fetchone()
         return render_template('about-employee.html',rs_employee=rows_rs_employee,count_employee=rows_count_employee)
 
-
 @app.route('/about_us')
 def about_us():
     with conn:
@@ -36,17 +35,28 @@ def about_us():
         rows = cur.fetchall()
         return render_template('about-us.html',datas=rows)
 
-
 @app.route('/time_log')
 def time_log():
-    with conn:
-        cur = conn.cursor()
-        cur.execute("SELECT * FROM employee_log_time LEFT JOIN employee_take_leave ON emptl_empid = empw_empid WHERE empw_date = CURDATE()")
-        rows_rs_employee = cur.fetchall()
-        cur.execute("SELECT DISTINCT empw_date FROM employee_log_time")
-        rows_rs_date = cur.fetchall()
-        times = ( timedelta(seconds=28800),timedelta(seconds=61200))
-        return render_template('time-log.html',rows_rs_employee=rows_rs_employee,times=times,rows_rs_date=rows_rs_date)
+        with conn:
+            cur = conn.cursor()
+            cur.execute("SELECT * FROM employee_log_time LEFT JOIN employee_take_leave ON emptl_empid = empw_empid WHERE empw_date = CURDATE()")
+            rows_rs_employee = cur.fetchall()
+            cur.execute("SELECT DISTINCT empw_date FROM employee_log_time")
+            rows_rs_date = cur.fetchall()
+            times = ( timedelta(seconds=28800),timedelta(seconds=61200))
+        return render_template('time-log.html',rows_rs_employee=rows_rs_employee,times=times,rows_rs_date=rows_rs_date,cur_date=date.today(),select_cur_date=date.today())
+
+@app.route('/time_log_search',methods=['POST'])
+def time_log_search():
+        empw_date = request.form['empw_date']
+        with conn:
+            cur = conn.cursor()
+            cur.execute("SELECT * FROM employee_log_time LEFT JOIN employee_take_leave ON emptl_empid = empw_empid WHERE empw_date = %s",(empw_date))
+            rows_rs_employee = cur.fetchall()
+            cur.execute("SELECT DISTINCT empw_date FROM employee_log_time")
+            rows_rs_date = cur.fetchall()
+            times = ( timedelta(seconds=28800),timedelta(seconds=61200))
+        return render_template('time-log.html',rows_rs_employee=rows_rs_employee,times=times,rows_rs_date=rows_rs_date,cur_date=empw_date,select_cur_date=date.today())
 
 @app.route('/calculate_salary')
 def calculate_salary():
@@ -79,7 +89,23 @@ def employee_delete(id_data):
     with conn.cursor() as cur:
         cur.execute("Delete from employee where emp_id = %s",(id_data))
         conn.commit()
-    return redirect(url_for('about_employee'))    
+    return redirect(url_for('about_employee')) 
+
+@app.route('/employee_update',methods=['POST'])
+def employee_update():
+    if request.method == "POST":
+        emp_id = request.form['emp_id']
+        emp_empid = request.form['emp_empid']
+        emp_fname = request.form['emp_fname']
+        emp_lname = request.form['emp_lname']
+        emp_address = request.form['emp_address']
+        emp_phone = request.form['emp_phone']
+        with conn.cursor() as cur:
+            sql="UPDATE employee SET emp_empid=%s,emp_fname=%s,emp_lname=%s,emp_address=%s,emp_phone=%s WHERE emp_id = %s"
+            cur.execute(sql,(emp_empid,emp_fname,emp_lname,emp_address,emp_phone,emp_id))
+            conn.commit()
+        return redirect(url_for('about_employee'))  
+
 
 if __name__ == "__main__":
     app.run(debug=True)
